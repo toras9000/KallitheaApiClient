74cb21b99c6fca2a465733f8929f04196514b7de
toras9000 <toras9000@example.com>
2023-07-30 12:08:39 +0900

    テストコンテナの起動スクリプトを変更

--- service/docker/assets/patched_startup.sh
+++ service/docker/assets/patched_startup.sh
@@ -9,10 +9,14 @@ PYTHON_PACKAGES=$(su-exec kallithea:kallithea $PYTHON_BIN -m site --user-site)
 # kallithea installation directory
 KALLITEHA_INSTALL_DIR=$PYTHON_PACKAGES/kallithea
 
-# overwrite patch files
-KALLITHEA_PATCH_DIR=/kallithea/assets/patch/kallithea
+# overwrite files
+if [ -d "$KALLITHEA_OVERRIDE_DIR/kallithea" ]; then
+    cp -RT "$KALLITHEA_PATCH_DIR/kallithea"  "$KALLITEHA_INSTALL_DIR"
+fi
+
+# patch files
 if [ -d "$KALLITHEA_PATCH_DIR" ]; then
-    cp -RT "$KALLITHEA_PATCH_DIR"  "$KALLITEHA_INSTALL_DIR"
+    git -C "$KALLITEHA_INSTALL_DIR" apply --reject --whitespace=fix -p2 $KALLITHEA_PATCH_DIR/*
 fi
 
 # normal startup
